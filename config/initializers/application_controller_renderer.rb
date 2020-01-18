527d103c1e655e6d21c1f302708174372ebed9a8
NodokaAoki <genta91nya@gmail.com>
2020-01-18 20:57:03 +0900

    [1/18Finish]Bookers2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2bf430fe5e97d02f1d34b0b8902e2860c58e9f065ad452270a226047f5738228754eca66de4cb335afb8d55fb2585bc9202e1c4ac7cc23843c7e9fc852201a3c'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  devise_for :users, controllers: {
+        sessions:      'users/sessions',
+        passwords:     'users/passwords',
+        registrations: 'users/registrations'
+    }
+
+    get 'home/index'
+    root to: "books#top"
+  resources :books, only: [:new, :create, :index, :show]
+  resources :users, only:[:show, :edit, :update, :index]
+    # For details on the DSL available within this file,
+    # see http://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
